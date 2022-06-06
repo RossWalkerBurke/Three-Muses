777932b978d0a103ec448882e32e4d674e897d23
RossWalkerBurke <58166708+RossWalkerBurke@users.noreply.github.com>
2022-06-06 22:26:11 +0100

    map tests and changes

--- Production/tiled/updatedtilesformap.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.5" name="updatedtilesformap" tilewidth="32" tileheight="32" tilecount="3588" columns="52">
- <image source="updatedtilesformap.png" width="1668" height="2224"/>
-</tileset>
