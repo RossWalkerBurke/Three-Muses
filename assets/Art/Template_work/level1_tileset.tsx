e56d795a5ddb64669f69e90827c5f00af1837e3d
RossWalkerBurke <58166708+RossWalkerBurke@users.noreply.github.com>
2022-06-07 19:20:26 +0100

    gitting up new map_1
    made a new map_1 by implementing new tileset.

---
+++ assets/Art/Template_work/level1_tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.5" name="level1_tileset" tilewidth="32" tileheight="32" tilecount="3588" columns="52">
+ <image source="level1_tileset.png" width="1668" height="2224"/>
+</tileset>
